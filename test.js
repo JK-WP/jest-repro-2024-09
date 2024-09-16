e33dd4b06701c90959b7e329602459b8e493ca87
JK-WP <92494241+JK-WP@users.noreply.github.com>
2024-09-16 13:38:21 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,7 +1,13 @@
 const repro = require('./repro.js');
 
 describe('jest bug', () => {
-    it('repro', async () => {
+    it('repro1', async () => {
         expect(await repro.didThrowSyntaxError()).toBe(true);
+    });
+
+    it('repro2', async () => {
+        await expect((new Response('not-valid-json')).json())
+            .rejects
+            .toEqual(SyntaxError('Unexpected token o in JSON at position 1'));
     });
 });
